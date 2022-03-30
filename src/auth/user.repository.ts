63b15da8685173e54b4e545ff6a85cd2ac01491f
HaeChan Kim <bluesun147@naver.com>
2022-03-30 22:03:34 +0900

    sign up

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,10 +1,17 @@
 // user entity를 db와 관련된 로직 처리하는 곳. 
 
 import { EntityRepository, Repository } from "typeorm";
+import { AuthCredentialsDto } from "./dto/auth-credential.dto";
 import { User } from "./user.entity";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
 
+    async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        const{username, password} = authCredentialsDto;
+        const user = this.create({ username, password });
+
+        await this.save(user);
+    }
 }
 
