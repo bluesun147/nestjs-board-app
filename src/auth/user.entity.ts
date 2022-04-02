7c4ae07ade88effe9161f5144fa6ca4e9aeb7d11
HaeChan Kim <bluesun147@naver.com>
2022-04-02 19:39:03 +0900

    user, board 관계 헝성 위한 entity 수정

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,6 +1,7 @@
 // 유저 엔티티
 
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
+import { Board } from "src/boards/board.entity";
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn, Unique } from "typeorm";
 
 @Entity()
 @Unique(['username'])
@@ -13,5 +14,10 @@ export class User extends BaseEntity {
 
     @Column()
     password: string;
+
+    // 관계 형성 위해서는 엔티티에 서로간의 필드 넣어줘야 함
+    // 타입, 보드에서 유저에 어떻게 접근하는지, 유저 정보 가져올 때 보드정보도 같이 가져온다는 뜻
+    @OneToMany(type => Board, board => board.user, {eager: true}) // 1:N relationship
+    boards: Board[] // 유저에 보드라는 컬럼 넣음. 여러개 넣을 수 있으니까 배열로
 }
 
--- src/boards/board.entity.ts
+++ src/boards/board.entity.ts
@@ -1,7 +1,8 @@
 // 엔티티 파일
 // 자동적으로 이러한 컬럼들 가진 보드 테이블 생성 (자동으로 테이블로 변환)
 
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "src/auth/user.entity";
+import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { BoardStatus } from "./board-status.enum";
 
 @Entity() // @Entity() 데코레이터 클래스는 Board 클래스가 엔티티임을 나타냄.
@@ -17,5 +18,10 @@ export class Board extends BaseEntity {
 
     @Column()
     status: BoardStatus;
+
+    // 관계 형성 위해서는 엔티티에 서로간의 필드 넣어줘야 함
+    // 타입, 유저에서 보드에 어떻게 접근하는지, 보드 정보 가져올 때 유저 정보는 가져오지 않는다는 뜻
+    @ManyToOne(type => User, user => user.boards, {eager: false}) // N:1 relationship
+    user: User;
 }
 
